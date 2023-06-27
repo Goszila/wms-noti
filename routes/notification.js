7e42f15fd2b2f5c47caf788e56fbe1c8518b1711
somsak.mun <somsak.mun@gfinthailand.com>
2023-06-27 13:39:42 +0700

    :sparkle: GET: notification

---
+++ routes/notification.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router()
+
+router.use((req, res, next) => {
+  next()
+})
+
+router.get('/', (req, res) => {
+  return res.status(200).send({
+    success: true,
+    payload: [
+      {
+        id: 1,
+        title: 'This AWB No has been created D/O',
+        doNumber: '669480',
+        mawbNo: '900-12345666',
+        hawbNo: 'SG100844711',
+        pathUrl: '/Delivery'
+      },
+      {
+        id: 2,
+        title: 'This AWB No has been created D/O',
+        doNumber: '669480',
+        mawbNo: '900-12345678',
+        hawbNo: 'SG1008447122',
+        pathUrl: '/Delivery'
+      },
+      {
+        id: 3,
+        title: 'This AWB No has been created D/O',
+        doNumber: '669480',
+        mawbNo: '900-12345633',
+        hawbNo: 'SG100844714',
+        pathUrl: '/Delivery'
+      },
+      {
+        id: 4,
+        title: 'This AWB No has been created D/O',
+        doNumber: '669480',
+        mawbNo: '900-12345655',
+        hawbNo: 'SG1008447165',
+        pathUrl: '/Delivery'
+      },
+      {
+        id: 5,
+        title: 'This AWB No has been created D/O',
+        doNumber: '669480',
+        mawbNo: '900-12345622',
+        hawbNo: 'SG1008447143',
+        pathUrl: '/Delivery'
+      }
+    ]
+  })
+})
+
+module.exports = router
